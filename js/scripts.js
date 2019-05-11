4335b89d5f95577975c1c0835c60fc9ff5eabd77
manishjoy <manishjoy1993@hotmail.com>
2019-05-11 18:53:37 +0530

    Raw Changes

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // PreLoader
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+
+
+    // --------------------------------------------------------------------
+    // Sticky Sidebar
+    // --------------------------------------------------------------------
+
+    $('.left-col-block, .right-col-block').theiaStickySidebar();
+
+    // --------------------------------------------------------------------
+    // For Mobile Titles
+    // --------------------------------------------------------------------
+    
+    $("[title]").click(function(e) {
+        // if ($(document).width() <= 768) {
+            $(".title-tooltip").remove();
+            let titleWidth = $(this).attr("title").length * 7.1;
+            $(this).append(
+                '<span class="title-tooltip">' + $(this).attr("title") + "</span>"
+            );
+        // }
+        
+    });
+
+    $(document).click(function(evt) {
+        var target = evt.target.className;
+        if(!(evt.target.hasAttribute('title') || (typeof $(evt.target).closest('[title]').attr('title') != 'undefined' && $(evt.target).closest('[title]').attr('title').length))) {
+            $(".title-tooltip").remove();
+        }
+    });
+}); // JQuery end
